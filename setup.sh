972cbfc5acc41762170e0fd70f4f4370a9862c56
meusebio.depaz <meusebio.depaz@bbva.com>
2016-10-07 10:39:08 +0200

    Requirements

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+node --version >/dev/null 2>&1 || { echo "I require node.js but it's not installed.  Aborting." >&2; exit 1; }
+git --version >/dev/null 2>&1 || { echo "I require git but it's not installed.  Aborting." >&2; exit 1; }
+docker --version >/dev/null 2>&1 || { echo "I require docker but it's not installed.  Aborting." >&2; exit 1; }
+docker-compose --version >/dev/null 2>&1 || { echo "I require docker-compose but it's not installed.  Aborting." >&2; exit 1; }
+
+docker pull thetallgrassnet/alpine-neo4j:2.3
+
+mkdir data
