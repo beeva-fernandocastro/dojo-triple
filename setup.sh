73fb712736d236ff553f2e45d724f566a60ec773
beeva-fernandocastro <nandofe2001@yahoo.es>
2016-10-08 11:43:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ git --version >/dev/null 2>&1 || { echo "I require git but it's not installed.  
 docker --version >/dev/null 2>&1 || { echo "I require docker but it's not installed.  Aborting." >&2; exit 1; }
 docker-compose --version >/dev/null 2>&1 || { echo "I require docker-compose but it's not installed.  Aborting." >&2; exit 1; }
 
-docker pull thetallgrassnet/alpine-neo4j:2.3
+sudo docker pull thetallgrassnet/alpine-neo4j:2.3
 
 mkdir data
